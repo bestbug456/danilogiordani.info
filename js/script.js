a5d8968c787e25a4a4d1ad9903cd58881de60528
bestbug456 <bestbug.corporation@gmail.com>
2014-10-04 12:16:58 +0200

    edited js

--- js/script.js
+++ js/script.js
@@ -4,154 +4,29 @@ $(document).ready(
         $('#attr1').fadeIn(1000);
 
         $('#showPortfolio').click(function(){
-
-            var divToFadeOut = ''
-
-            
-            if($('#portfolio').css("display") == 'block'){
-              divToFadeOut = '#portfolio';
-            }
-            if($('#changefix').css("display") == 'block'){
-              divToFadeOut = '#changefix';
-            }
-            if($('#scuola').css("display") == 'block'){
-              divToFadeOut = '#scuola';
-            }
-            if($('#lavori').css("display") == 'block'){
-              divToFadeOut = '#lavori';
-            }
-            if($('#contatti').css("display") == 'block'){
-              divToFadeOut = '#contatti';
-            }
-            
-            if (divToFadeOut != '') {
-              $(divToFadeOut).fadeOutLeft(1000);
-            };
-            $('#portfolio').fadeInRight(1000);
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-                $("html, body").animate({ scrollTop: 510 }, 5000);
-            }
-
-            
+            $('html, body').animate({
+           'scrollTop':   $('#anchorPortfolio').offset().top
+         }, 2000);
         });
-        $('#showChangefix').click(function(){
-            var divToFadeOut = ''
-
-            
-            if($('#portfolio').css("display") == 'block'){
-              divToFadeOut = '#portfolio';
-            }
-            if($('#changefix').css("display") == 'block'){
-              divToFadeOut = '#changefix';
-            }
-            if($('#scuola').css("display") == 'block'){
-              divToFadeOut = '#scuola';
-            }
-            if($('#lavori').css("display") == 'block'){
-              divToFadeOut = '#lavori';
-            }
-            if($('#contatti').css("display") == 'block'){
-              divToFadeOut = '#contatti';
-            }
-            
-            if (divToFadeOut != '') {
-              $(divToFadeOut).fadeOutLeft(1000);
-            };
-
-            
-
-            $('#changefix').fadeInRight(1000);
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-                $("html, body").animate({ scrollTop: 510 }, 5000);
-            }
-        });
-
         $('#showLavori').click(function(){
-            var divToFadeOut = ''
-
-            
-            if($('#portfolio').css("display") == 'block'){
-              divToFadeOut = '#portfolio';
-            }
-            if($('#changefix').css("display") == 'block'){
-              divToFadeOut = '#changefix';
-            }
-            if($('#scuola').css("display") == 'block'){
-              divToFadeOut = '#scuola';
-            }
-            if($('#lavori').css("display") == 'block'){
-              divToFadeOut = '#lavori';
-            }
-            if($('#contatti').css("display") == 'block'){
-              divToFadeOut = '#contatti';
-            }
-            
-            if (divToFadeOut != '') {
-              $(divToFadeOut).fadeOutLeft(1000);
-            };
-
-            $('#lavori').fadeInRight(1000);
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-                $("html, body").animate({ scrollTop: 510 }, 5000);
-            }
+            $('html, body').animate({
+           'scrollTop':   $('#anchorLavori').offset().top
+         }, 2000);
         });
         $('#showScuola').click(function(){
-            var divToFadeOut = ''
-            if($('#portfolio').css("display") == 'block'){
-              divToFadeOut = '#portfolio';
-            }
-            if($('#changefix').css("display") == 'block'){
-              divToFadeOut = '#changefix';
-            }
-            if($('#scuola').css("display") == 'block'){
-              divToFadeOut = '#scuola';
-            }
-            if($('#lavori').css("display") == 'block'){
-              divToFadeOut = '#lavori';
-            }
-            if($('#contatti').css("display") == 'block'){
-              divToFadeOut = '#contatti';
-            }
-            
-            if (divToFadeOut != '') {
-              $(divToFadeOut).fadeOutLeft(1000);
-            };
-
-            
-
-            $('#scuola').fadeInRight(1000);
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-                $("html, body").animate({ scrollTop: 310 }, 5000);
-            }
+            $('html, body').animate({
+           'scrollTop':   $('#anchorScuola').offset().top
+         }, 2000);
         });
         $('#showContatti').click(function(){
-            var divToFadeOut = ''
-            if($('#portfolio').css("display") == 'block'){
-              divToFadeOut = '#portfolio';
-            }
-            if($('#changefix').css("display") == 'block'){
-              divToFadeOut = '#changefix';
-            }
-            if($('#scuola').css("display") == 'block'){
-              divToFadeOut = '#scuola';
-            }
-            if($('#lavori').css("display") == 'block'){
-              divToFadeOut = '#lavori';
-            }
-            if($('#contatti').css("display") == 'block'){
-              divToFadeOut = '#contatti';
-            }
-            
-            if (divToFadeOut != '') {
-              $(divToFadeOut).fadeOutLeft(1000);
-            };
-
-            
-
-            $('#contatti').fadeInRight(1000);
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-                $("html, body").animate({ scrollTop: 510 }, 5000);
-            }
+            $('html, body').animate({
+           'scrollTop':   $('#anchorContatti').offset().top
+         }, 2000);
+        });
+        $('#showChangefix').click(function(){
+            $('html, body').animate({
+           'scrollTop':   $('#anchorChangefix').offset().top
+         }, 2000);
         });
 
         var $element = '#attr';
@@ -173,16 +48,18 @@ $(document).ready(
                 $actualEle.fadeOut(1500);
                 $nextEle.delay(1500).fadeIn(1500);
             }
-               // $actualEle.toggle("slide").animate({ opacity: 0 }, 1000);
-               // $nextEle.animate({ opacity: 1 }, 1000).show("slide", { direction: "right" }, 1000);
         }, 4000);
         
     }
 );
 
-//WIP
 
+//WIP
+/*
 (function ($) {
+
+
+
   var getUnqueuedOpts = function (opts) {
     return {
       queue: false,
@@ -193,13 +70,13 @@ $(document).ready(
   $.fn.fadeOutLeft = function (opts) {
     opts = opts || {};
     $(this).show().slideUp(opts).animate({ opacity: 0 }, getUnqueuedOpts(opts));
-    /*hide().slideDown(opts).animate({ opacity: 1 }, getUnqueuedOpts(opts));*/
+    /*hide().slideDown(opts).animate({ opacity: 1 }, getUnqueuedOpts(opts));
   };
   $.fn.fadeInRight = function (opts) {
     opts = opts || {};
     $(this).animate({ opacity: 1 }, opts).show("slide", { direction: "right" }, opts);
     /*show("slide", { direction: "right" }, opts).animate({ opacity: 1 }, opts);
-    /*show().slideUp(opts).animate({ opacity: 0 }, getUnqueuedOpts(opts));*/
+    /*show().slideUp(opts).animate({ opacity: 0 }, getUnqueuedOpts(opts));
   };
   $.fn.verticalFade = function (opts) {
     opts = opts || {};
@@ -209,7 +86,7 @@ $(document).ready(
       $(this).showDown(opts);
     }
   };
-}(jQuery));
+}(jQuery));*/
 
 
 
