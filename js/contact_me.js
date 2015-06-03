0201cd5257065dc377f118f6f2b2cea4cb9fb1e1
bestbug <bestbug.corporation@gmail.com>
2015-06-03 20:07:56 +0200

    Fix bug about mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/bestbug.corporation@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
